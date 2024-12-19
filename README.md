7f9c4e86c819fea0c9c36cca7c519bf02b0279f5
Leonardo Vinicius Batista Santos <leonardovinicius9987@gmail.com>
2024-12-19 00:59:56 +0000

    update mensage

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # forum
 An interactive forum. Inspired by TabNews, this platform aims to foster idea exchange, knowledge sharing, and collaborative discussions.
-
-é o Albérico
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Olá, tudo bem com vocês?</h1>
+    return <h1>Oi amor da minha vida, você sabe que eu amo você mais do que tudo na minha vida? <br/>Se você me ama também, me dá uma ridadinha neném</h1>
 }
 
 export default Home;
