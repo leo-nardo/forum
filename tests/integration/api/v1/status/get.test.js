1b34052afbba7bb78525eaacec643fc693bc4817
Leonardo Vinicius Batista Santos <leonardovinicius9987@gmail.com>
2025-01-10 03:47:38 +0000

    finish `api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,16 @@
 test("Get to api/v1/status should return 200", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status")
-    expect(response.status).toBe(200)
-})
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+
+  const parsedVersion = parseFloat(responseBody.dependecies.database.version);
+  expect(typeof parsedVersion).toBe("number");
+  expect(parsedVersion).toBe(16);
+
+  expect(responseBody.dependecies.database.max_connections).toBe(100);
+  expect(responseBody.dependecies.database.opened_connection).toBe(1);
+});
