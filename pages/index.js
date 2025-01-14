6bfa9a17abfd3b980c1b3c133f454e510abed8ca
Leonardo Vinicius Batista Santos <leonardovinicius9987@gmail.com>
2025-01-14 19:20:44 +0000

    atualização da página principal

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <p>endpoint´s finalizados: https://papoacademico.com.br/api/v1/status</p>
+    <p>endpoint´s finalizados: https://papoacademico.com.br/api/v1/status e https://papoacademico.com.br/api/v1/migrations</p>
   );
 }
 
