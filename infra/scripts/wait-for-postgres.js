06935cf43cb5d4c27e4e006410f098e056c10ac0
Leonardo Vinicius Batista Santos <leonardovinicius9987@gmail.com>
2025-04-09 13:08:25 +0000

    adds  script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,19 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+
+    process.stdout.write("\n🟢postgres está pornto e aceitando conexões!\n");
+  }
+}
+
+process.stdout.write("\n\n🔴Aguargando Postgres aceitar conexões");
+
+checkPostgres();
